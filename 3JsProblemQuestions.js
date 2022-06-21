20b9f2d41c7e0f4bd0f1ad9b0d858ca582820506
sikumiku <sigridaasma@gmail.com>
2022-06-22 00:26:58 +0300

    solution to q2

--- 3JsProblemQuestions.js
+++ 3JsProblemQuestions.js
@@ -39,11 +39,23 @@ function cleanRoom(array) {
     return groupedArray;
 }
 
+console.log(cleanRoom(input))
 
 // Question 2: Write a javascript function that takes an array of numbers 
 //and a target number. The function should find two different numbers in the 
 // array that, when added together, give the target number. 
 // For example: answer([1,2,3], 4)should return [1,3]
+
+function findNumbers(array, targetNumber) {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] === targetNumber) {
+                return [array[i], array[j]]
+            }
+        }
+    }
+    return [];
+}
 
 // Question 3: Write a function that converts HEX to RGB. Then Make that 
 // function auto-dect the formats so that if you enter HEX color format it 
