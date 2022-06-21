e4c0d1dc64ec48006bf9921bed53f1f1ad8f326c
sikumiku <sigridaasma@gmail.com>
2022-06-22 01:32:03 +0300

    q3 bonus solution

--- 3JsProblemQuestions.js
+++ 3JsProblemQuestions.js
@@ -88,8 +88,46 @@ const convertHexValueToNumber = (hexElement) => {
     }
 }
 
+const convertNumberToHexValue = (number) => {
+    if (number < 10) {
+        return parseInt(number, 10);
+    }
+    switch(number) {
+        case 10 :
+            return "A";
+        case 11 :
+            return "B";
+        case 12 :
+            return "C";
+        case 13 :
+            return "D";
+        case 14 :
+            return "E";
+        case 15 :
+            return "F";
+        default:
+            throw `Invalid number ${number} given to calculate hex!`;
+    }
+}
+
 const calcDec = (val1, val2) => {
     return 16 * val1 + val2
+}
+
+const determineNumberFormat = (numberString) => {
+    if (numberString.length > 6) {
+        return "rgb"
+    } else {
+        return "hex"
+    }
+}
+
+function convertNumber(input) {
+    if (determineNumberFormat(input) === "hex") {
+        return convertHexToRgb(input)
+    } else {
+        return convertRgbToHex(input)
+    }
 }
 
 function convertHexToRgb(hex) {
@@ -108,6 +146,17 @@ function convertHexToRgb(hex) {
         }
     });
     return rgbValueArray.join(",");
+}
+
+function convertRgbToHex(rgb) {
+    let hex = "";
+    rgb.split(",").forEach(rgbValue => {
+        let a = rgbValue / 16;
+        let firstValue = Math.floor(a)
+        let secondValue = Math.floor((a % 1).toFixed(2)*16)
+        hex = hex.concat(convertNumberToHexValue(firstValue)).concat(convertNumberToHexValue(secondValue))
+    });
+    return hex;
 }
 
 // format of hex AA1122, format of rgb 255,204,153
